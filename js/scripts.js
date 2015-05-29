87ecd88f0b2d3082d6cb379239b51e28810baa64
fightypants <kha.les.email@gmail.com>
2015-05-29 10:42:18 -0700

    Will create and pass spec to find and replace one word in a sentence

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+      // This logic is unused but was attempted to solve by using array was
+      // abandoned due for simple soulotion below
+// var findReplace = function(sentence,word,newWord) {
+//   var new_array = sentence.split()
+//   for (var i = 0; i >= new_array.length; i++) {
+//     if (sentence[i] === word) {
+//       sentence[i].replace(/word/g, newWord)
+//     }
+//   };
+//   return new_array
+// };
+
+
+
+var replacer = function(sentence,word,newWord) {
+  var text = sentence
+  var new_text = sentence.replace(word,newWord)
+  return new_text
+};
---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('replacer', function(){
+  it('is able to find a word in a string and replace it', function() {
+    expect(replacer('hello world', 'hello', 'bye')).to.eql('bye world')
+  });
+
+  it('is able to find multiple same words and replace all of them')
+
+});
